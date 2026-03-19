6233edd89a2e37cbea9f0ce71427bb8668aaa603
Gabriel Medina Piñeyro <gmp1120@hotmail.com>
2026-03-19 00:16:41 -0300

    Añado otras operaciones
    y aprendo a usar parámetro de función e y llamar por e.target.id. Me planteo que camino seguir para hacer la calculadora cientifica en un futuro, y comienzo a validar datos de forma muy basica, poniendo un if dentro del if else de la division, para que al dividir entre 0 salte una alerta basica; luego hare esa alerta con un div mas prolijo. Tambien aprendo la existencia de switchpara programar una calculadora; y lo fundamental es que erradique el uso de var para escribir con const y let.

--- practica.js
+++ practica.js
@@ -1,17 +1,40 @@
 
-var operaciones = function() {
+const operaciones = function(e) {
 
-    var numero1 = parseFloat(document.getElementById("1rstnumber").value);
-    var numero2 = parseFloat(document.getElementById("2ndnumber").value);
-    var result = document.getElementById("result");
+    const numero1 = parseFloat(document.getElementById("1rstnumber").value);
+    const numero2 = parseFloat(document.getElementById("2ndnumber").value);
+    const result = document.getElementById("resultado");
+    const operacion = e.target.id;
+    let resultado;
 
-    var ResultadoFinal = numero1 + numero2;
+    if (operacion == "suma") {
 
-    result.innerHTML = "The result is:" + ResultadoFinal;
+         resultado = numero1 + numero2;
+    }
+    else if (operacion == "resta") {
 
-}
+         resultado = numero1 - numero2;
+    }
+    else if (operacion == "multiplicacion") {
 
-var boton = document.getElementById("boton");
+         resultado = numero1 * numero2;
+    }
+    else if (operacion == "division") {
+
+        if (numero2 == 0) {
+            alert("No se puede dividir entre 0");
+            return;
+        }
+         resultado = numero1 / numero2;
+    }
+
+    result.innerHTML = resultado;
+
+
+
+};
+
+const boton = document.getElementById("botones");
 boton.addEventListener("click", operaciones);
 
 
